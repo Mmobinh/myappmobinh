aa895710eb5584145ab8c15e5b5fd5276a142f4b
Mmobinh <98909174+Mmobinh@users.noreply.github.com>
2025-08-08 01:32:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,35 @@ COUNTRIES = {
     },
     "smsbower": {
         "Kazakhstan": 2,
+      "Iran": 57,
+    "Russia": 0,
+    "Ukraine": 1,
+    "Mexico": 54,
+    "Italy": 86,
+    "Spain": 56,
+    "Czech Republic": 63,
+    "Kazakhstan": 2,
+    "Paraguay": 87,
+    "Hong Kong": 14,
+    "macao": 20,
+    "irland": 23,
+    "serbia": 29,
+    "romani": 32,
+    "estonia": 34,
+    "germany": 43,
+    "auustria": 50,
+    "belarus": 51,
+    "tiwan": 55,
+    "newziland": 67,
+    "belgium": 82,
+    "moldova": 85,
+    "armenia": 148,
+    "maldiv": 159,
+    "guadlouap": 160,
+    "denmark": 172,
+    "norway": 174,
+    "switzerland": 173,
+    "giblarator": 201,
         # Add more countries here
     },
     "tiger": {
@@ -362,5 +391,6 @@ async def main():
 if __name__ == "__main__":
     nest_asyncio.apply()
     asyncio.run(main())
+
 
 
